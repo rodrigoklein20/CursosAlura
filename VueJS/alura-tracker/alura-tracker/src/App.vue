97e1a6df7bd429c493e914bba01a9c09e1d3618d
rodrigoklein20 <kleinrodrigo12@gmail.com>
2023-07-24 20:21:44 -0300

    Estudando VUE

--- VueJS/alura-tracker/alura-tracker/src/App.vue
+++ VueJS/alura-tracker/alura-tracker/src/App.vue
@@ -1,25 +1,73 @@
 <template>
-  <main class="columns is-gapless is-multiline">
+  <main class="columns is-gapless is-multiline" :class="{ 'modo-escuro': modoEscuroAtivo}">
     <div class="column is-one-quarter">
-      <BarraLateral></BarraLateral>
+      <BarraLateral @aoTemaAlterado="trocarTema"></BarraLateral>
     </div>
-    <div class="column is-three-quarter">
+    <div class="column is-three-quarter conteudo">
+      <FormularioVue @aoSalvarTarefa="salvarTarefa"></FormularioVue>
+      <div class="lista">
+        <TarefaVue
+          v-for="tarefa in tarefas"
+          :key="tarefa.descricao"
+          :tarefa="tarefa"
+        />
 
+        <BoxVue v-if="listaEstaVazia"> Você não está muito produtivo hoje </BoxVue>
+      </div>
     </div>
   </main>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
+import { defineComponent } from "vue";
 
-import BarraLateral from './components/BarraLateral.vue';
+import BarraLateral from "./components/BarraLateral.vue";
+import FormularioVue from "./components/FormularioVue.vue";
+import TarefaVue from "./components/TarefaVue.vue";
+import ITarefa from "./interfaces/ITarefa";
+import BoxVue from "./components/BoxVue.vue";
 
 export default defineComponent({
-    name: "App",
-    components: { BarraLateral }
+  name: "App",
+  components: { BarraLateral, FormularioVue, TarefaVue, BoxVue },
+  data() {
+    return {
+      tarefas: [] as ITarefa[],
+      modoEscuroAtivo: false,
+    };
+  },
+  computed: {
+    listaEstaVazia () : boolean {
+      return this.tarefas.length === 0
+    }
+  },
+  methods: {
+    salvarTarefa(tarefa: ITarefa) {
+      this.tarefas.push(tarefa);
+    },
+    trocarTema (modoEscuroAtivo : boolean) {
+      this.modoEscuroAtivo = modoEscuroAtivo
+    }
+  },
 });
 </script>
 
 <style>
+.lista {
+  padding: 1.25rem;
+}
 
+main {
+  --bg-primario: #fff;
+  --texto-primario: #000;
+}
+
+main.modo-escuro {
+  --bg-primario: #2b2d42;
+  --texto-primario: #ddd;
+}
+
+.conteudo {
+  background-color: var(--bg-primario);
+}
 </style>
---
+++ VueJS/alura-tracker/alura-tracker/src/components/TarefaVue.vue
@@ -0,0 +1,39 @@
+<template>
+    <BoxVue>
+        <div class="columns">
+            <div class="column is-7">
+                {{ tarefa.descricao || 'Tarefa sem descrição' }}
+            </div>
+            <div class="column">
+                <CronometroVue :tempoEmSegundos="tarefa.duracaoEmSegundos"/>
+            </div>
+        </div>
+    </BoxVue>
+</template>
+
+<script lang="ts">
+import { defineComponent, PropType } from 'vue';
+import CronometroVue from './CronometroVue.vue';
+import ITarefa from '../interfaces/ITarefa';
+import BoxVue from './BoxVue.vue';
+
+export default defineComponent ({
+    name: 'TarefaVue',
+    components: {
+        CronometroVue,
+        BoxVue
+    },
+    props: {
+        tarefa: {
+            type: Object as PropType<ITarefa>,
+            required: true        
+        }
+    }
+})
+</script>
+
+<style scoped>
+.box {
+    background: #faf0ca;
+}
+</style>
